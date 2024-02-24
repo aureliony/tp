16b0edb7dce478c36e2174e734f9c5f244dbf555
aureliony <39163684+aureliony@users.noreply.github.com>
2024-02-24 21:28:58 +0800

    Update all other usages of Person

--- src/main/java/seedu/address/model/person/Remark.java
+++ src/main/java/seedu/address/model/person/Remark.java
@@ -7,11 +7,24 @@ import static java.util.Objects.requireNonNull;
  * Guarantees: immutable; is always valid
  */
 public class Remark {
+
+    public static final String MESSAGE_CONSTRAINTS = "Remarks can take any values";
+
+    /*
+     * The first character of the address must not be a whitespace,
+     * otherwise " " (a blank string) becomes a valid input.
+     */
+    public static final String VALIDATION_REGEX = "(?s).*";
+
     public final String value;
 
     public Remark(String remark) {
         requireNonNull(remark);
         value = remark;
+    }
+
+    public static boolean isValidRemark(String test) {
+        return test.matches(VALIDATION_REGEX);
     }
 
     @Override
